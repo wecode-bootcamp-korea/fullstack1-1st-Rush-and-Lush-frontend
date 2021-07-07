734d530cecdca0be432de144c23c932a48b3e49a
hxyxneee <wlgus3096@gmail.com>
2021-07-07 23:37:41 +0900

    Add: Navigration bar - 검색, 장바구니, 유저 아이콘 추가

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,8 +1,12 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-import Product from "./Component/LushProducts/Products";
-import LushIntro from "./Component/LushIntro/Lushintro";
+import Product from "./Component/LushProducts/LushProducts";
+import LushIntro from "./Component/LushIntro/LushIntro";
 import "./Nav.scss";
+import { IoMdSearch } from "react-icons/io";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { BsPersonFill } from "react-icons/bs";
+import { RiCreativeCommonsZeroLine } from "react-icons/ri";
 
 const CATEGORY = ["매장 안내", "스파", "이벤트"];
 
@@ -27,7 +31,28 @@ class Nav extends Component {
               </li>
             ))}
           </ul>
-          <div className="headerIcon"></div>
+          <div className="headerIcon">
+            <ul>
+              <li className="navIcon">
+                <Link className="finder" to="/">
+                  <IoMdSearch />
+                </Link>
+              </li>
+              <li className="navIcon2">
+                <Link className="shopping" to="/">
+                  <HiOutlineShoppingBag />
+                </Link>
+                <span className="statusZero">
+                  <RiCreativeCommonsZeroLine />
+                </span>
+              </li>
+              <li className="navIcon3">
+                <Link className="personal" to="/">
+                  <BsPersonFill />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
