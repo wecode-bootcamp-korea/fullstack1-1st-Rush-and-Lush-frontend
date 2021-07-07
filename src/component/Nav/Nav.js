d8e0b33bdc44656e003b4281edc66661b5967d9a
hxyxneee <wlgus3096@gmail.com>
2021-07-07 21:37:29 +0900

    Add: Navigation 레이아웃 진행 중 - Component 제외

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,9 +1,36 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
+import Product from "./Component/LushProducts/Products";
+import LushIntro from "./Component/LushIntro/Lushintro";
 import "./Nav.scss";
+
+const CATEGORY = ["매장 안내", "스파", "이벤트"];
 
 class Nav extends Component {
   render() {
-    return <p> 네비게이션 페이지</p>;
+    return (
+      <nav>
+        <div className="nav">
+          <div className="logo">
+            <Link className="mainLogo" to="/main">
+              WESH
+            </Link>
+          </div>
+          <ul className="category">
+            <Product />
+            <LushIntro />
+            {CATEGORY.map(category => (
+              <li className="tooltip">
+                <Link className="navTitle" to="/">
+                  {category}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="headerIcon"></div>
+        </div>
+      </nav>
+    );
   }
 }
 export default Nav;
