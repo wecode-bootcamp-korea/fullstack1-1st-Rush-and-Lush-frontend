20dafebeccf72db8ef5ec96550df74e5db08264f
hxyxneee <wlgus3096@gmail.com>
2021-07-08 22:05:07 +0900

    Add: Footer 레이아웃 진행 중

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,9 +1,38 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
 import "./Footer.scss";
+
+const CATEGORY = [
+  "스카우트",
+  "회사소개",
+  "개인정보처리방침",
+  "영상정보관리지침",
+  "이용약관",
+  "고객센터",
+];
 
 class Footer extends Component {
   render() {
-    return <p> footer 페이지</p>;
+    return (
+      <footer className="Footer">
+        <div className="footer">
+          <div className="logo">
+            <ul className="footerCategory">
+              <Link className="footerMainLogo" to="/main">
+                WESH
+              </Link>
+              {CATEGORY.map(el => (
+                <li className="footerLink" key={el.id}>
+                  <Link className="footerCategoryLink" to="/">
+                    {el}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </footer>
+    );
   }
 }
 export default Footer;
