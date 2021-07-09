031b7bf2ed2ba296fd59563a6a23f3ce4d193f7f
hxyxneee <wlgus3096@gmail.com>
2021-07-09 18:43:32 +0900

    Refactor: key name - camelCase로 변경

---
+++ src/component/Nav/Component/UserToolTip/UserToolTip.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+const USER_TOOLTIP = [
+  { link: "/login", subTitle: "로그인" },
+  { link: "/signup", subTitle: "회원가입" },
+  { link: "/", subTitle: "스카우트" },
+  { link: "/", subTitle: "고객센터" },
+];
+
+class UserToolTip extends Component {
+  render() {
+    return (
+      <div className="UserToolTip">
+        <div className="userToolTip">
+          {USER_TOOLTIP.map(el => {
+            return (
+              <div key={el.id} className="userToolTipList">
+                <Link className="usersLink" to={el.link}>
+                  {el.subTitle}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserToolTip;
