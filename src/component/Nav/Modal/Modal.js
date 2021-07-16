c393ea3f8665db3eb7ce3c0f3cb9d8d33973e2ff
samankey <71953671+samankey@users.noreply.github.com>
2021-07-16 10:15:11 +0900

    Fix : 모달창 start 하자마자 뜨는 오류 해결

--- src/component/Nav/Modal/Modal.js
+++ src/component/Nav/Modal/Modal.js
@@ -3,16 +3,9 @@ import { IoSearch } from "react-icons/io5";
 import "./Modal.scss";
 
 class Modal extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     modalVisible: !this.props.modalVisible,
-  //   };
-  // }
-
   render() {
     return (
-      <div className={this.props.modalVisible ? "hideModal" : "Modal"}>
+      <div className={this.props.modalVisible ? "Modal" : "hideModal"}>
         <h1>지금 많이 찾는 제품</h1>
         <form>
           <input type="text" placeholder="#위코드풀스택  #1기화이팅" />
