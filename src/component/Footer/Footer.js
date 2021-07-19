952075684335c4291a4ae1f074e386d4b16007ae
hxyxneee <wlgus3096@gmail.com>
2021-07-19 19:29:19 +0900

    Refactor: Footer - 전반적인 코드 리팩토링

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -16,11 +16,22 @@ const CATEGORY = [
   "고객센터",
 ];
 
+const FOOTER_ICONS = [
+  <FaFacebookSquare />,
+  <FaInstagramSquare />,
+  <FaYoutubeSquare />,
+  <FaPinterestSquare />,
+  <FaTumblrSquare />,
+];
+
+const KEY_CODE = 13;
+
 class Footer extends Component {
   constructor() {
     super();
     this.state = {
       inputEmail: "",
+      isFooterModalOn: false,
     };
   }
 
@@ -29,7 +40,7 @@ class Footer extends Component {
   };
 
   onKeyDown = keyEvent => {
-    if ((keyEvent.charCode || keyEvent.keyCode) === 13) {
+    if ((keyEvent.charCode || keyEvent.keyCode) === KEY_CODE) {
       keyEvent.preventDefault();
     }
   };
@@ -41,17 +52,25 @@ class Footer extends Component {
       event.target.reset();
     } else if (inputUserEmail) {
       alert("현재 지원되지 않는 기능입니다. 조금만 기다려주세요! ⏰");
-    } else {
-      return;
     }
   };
 
   render() {
     const { inputEmail } = this.state;
     const { handleInput, onKeyDown, subScribeByEmail } = this;
+    const USER_VALIDATION = {
+      isValidatedEmail: () =>
+        inputEmail.includes("@") && inputEmail.length >= 8,
+    };
+
     return (
       <footer className="Footer">
-        <div className="iconWesh"></div>
+        <div className="iconWesh">
+          <img
+            src="http://img.lush.co.kr/images/core/footer-icons.svg"
+            alt="러쉬 하단 이미지"
+          ></img>
+        </div>
         <div className="footerWrap">
           <div className="footer">
             <div className="subWrap">
@@ -76,20 +95,20 @@ class Footer extends Component {
                 <dd className="lushEmail">wesh@order.com</dd>
               </dl>
               <div className="iconWrap">
-                <FaFacebookSquare className="icon" />
-                <FaInstagramSquare className="icon" />
-                <FaYoutubeSquare className="icon" />
-                <FaPinterestSquare className="icon" />
-                <FaTumblrSquare className="icon" />
+                {FOOTER_ICONS.map((icons, index) => (
+                  <span className="icon" key={index}>
+                    {icons}
+                  </span>
+                ))}
               </div>
             </div>
             <div className="subWrapRight">
               <div className="footerCategory">
                 <ul>
-                  {CATEGORY.map((el, index) => (
+                  {CATEGORY.map((category, index) => (
                     <li className="footerLink" key={index}>
                       <Link className="footerCategoryLink" to="/">
-                        {el}
+                        {category}
                       </Link>
                     </li>
                   ))}
@@ -112,7 +131,7 @@ class Footer extends Component {
                   <button
                     type="submit"
                     className={
-                      inputEmail.includes("@") && inputEmail.length >= 8
+                      USER_VALIDATION.isValidatedEmail()
                         ? "emailBtn active"
                         : "emailBtn "
                     }
@@ -143,4 +162,5 @@ class Footer extends Component {
     );
   }
 }
+
 export default Footer;
