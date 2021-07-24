e95a74321a7eae14fcad644ac8b96679e68797ff
samankey <71953671+samankey@users.noreply.github.com>
2021-07-24 12:44:18 +0900

    FIx : Info 비활성화

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,10 +1,10 @@
 import { Component } from "react";
 import DetailNav from "./DetailNav/DetailNav";
 import Banner from "./Banner/Banner";
-import Info from "./Info/Info";
-// import ItemTab from "./ItemTab/ItemTab";
+// import Info from "./Info/Info";
+import ItemTab from "./ItemTab/ItemTab";
 import ItemDetail from "./ItemDetail/ItemDetail";
-// import ItemTab2 from "./ItemTab2/ItemTab2";
+import ItemTab2 from "./ItemTab2/ItemTab2";
 import Review from "./Review/Review";
 import "./Detail.scss";
 
@@ -33,7 +33,7 @@ class Detail extends Component {
           <DetailNav />
           <div className="detailInfoWrap">
             <Banner />
-            {this.state.products.map(
+            {/* {this.state.products.map(
               ({ img, name, price, weight, description }, index) => {
                 return (
                   <Info
@@ -46,12 +46,12 @@ class Detail extends Component {
                   />
                 );
               }
-            )}
+            )} */}
           </div>
         </div>
-        {/* <ItemTab /> */}
+        <ItemTab />
         <ItemDetail />
-        {/* <ItemTab2 /> */}
+        <ItemTab2 />
         <Review />
       </div>
     );
